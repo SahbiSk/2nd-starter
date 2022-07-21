221aea53c7eaea35da2d6248e2ca29a1a1d65bbe
LAPTOP-SBARKALL\DELL <sbarkallah@anypli.com>
2022-07-21 12:47:55 +0200

    6 commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import kiwi from './assets/static/kiwi.svg'
 
 const App = () => {
   const [counter, setCounter] = useState<number>(0)
+  const name = 'sahbi'
   return (
     <div>
       <img width={200} src={react} alt="react" />
       <img width={200} src={kiwi} alt="kiwi" />
       <h1>{process.env.NODE_ENV}!!z</h1>
+      <h1>{name}!!</h1>
       <h1>{process.env.CC}</h1>
       <button onClick={() => setCounter((p) => p + 1)}> {counter} </button>
     </div>
