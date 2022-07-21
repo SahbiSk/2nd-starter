76b0a822a0c2f22db3b07bd095e2ed520b6787c3
LAPTOP-SBARKALL\DELL <sbarkallah@anypli.com>
2022-07-21 12:42:41 +0200

    2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import kiwi from './assets/static/kiwi.svg'
 
 const App = () => {
   const [counter, setCounter] = useState<number>(0)
+  const ssss="jj"
   return (
     <div>
       <img width={200} src={react} alt="react" />
