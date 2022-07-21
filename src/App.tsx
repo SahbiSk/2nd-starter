1fe49c5aa97433bb00bfa6156804639a9b2c6b2b
LAPTOP-SBARKALL\DELL <sbarkallah@anypli.com>
2022-07-21 12:37:52 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import react from './assets/static/icon.png'
+import kiwi from './assets/static/kiwi.svg'
+
+const App = () => {
+  const [counter, setCounter] = useState<number>(0)
+  return (
+    <div>
+      <img width={200} src={react} alt="react" />
+      <img width={200} src={kiwi} alt="kiwi" />
+      <h1>{process.env.NODE_ENV}!!z</h1>
+      <h1>{process.env.CC}</h1>
+      <button onClick={() => setCounter((p) => p + 1)}> {counter} </button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+const container = document.getElementById('root')
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!) // createRoot(container!) if you use TypeScript
+root.render(<App />)
